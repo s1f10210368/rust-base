3a44caa1b8d4505f29798e3617dd55d68744e8fe
s1f10210368 <s1f102103684@iniad.org>
2023-12-12 20:46:54 +0900

    failed rocket

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,16 @@
-// Fizzbuzzとは1から指定された上限までの数を数え上げて
-// 各数に対して特定の条件を満たす場合に特定のメッセージを返すもの
+// rocketはリクエストを受け付け、処理を実施した後にレスポンスを返すWebフレームワーク
 
-pub mod rectangle;
+#[macro_use] extern crate rocket;
 
-fn main() {
-    fizzbyzz_to(30);
-    rectangle::rectangle_main();
+#[get("/hello")]                // <-- ルート
+fn index() -> &'static str {    // <-- ハンドラ
+    "Hello, Rocket"
 }
 
-// 引数の後には必ず型指定
-// 戻り値は -> の後ろに型を指定
 
-fn is_divisible_by(x: u32, y: u32) -> bool {
-    if y == 0 {
-        return false;
-    }
-    x % y == 0
-}
-
-fn fizzbuzz(n: u32) -> () {
-    if is_divisible_by(n, 15) {
-        println!("{}: fizzbuzz", n);
-    } else if is_divisible_by(n, 3) {
-        println!("{}: fizz", n)
-    } else if is_divisible_by(n, 5) {
-        println!("{}: buzz", n)
-    } else {
-        println!("{}: ()", n)
-    }
-}
-
-fn fizzbyzz_to(upper_limit: u32) {
-    for e in 1..=upper_limit {
-        fizzbuzz(e)
-    }
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+    .mount("/", routes![index])
+    .mount("/test", routes![index])
 }
