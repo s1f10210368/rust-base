eef95e2833c22d29ca29ee24fa787e8bc478e8e9
s1f10210368 <s1f102103684@iniad.org>
2023-12-12 19:58:22 +0900

    rectangle

---
+++ Fizzbuzz/rectangle.rs
@@ -0,0 +1,23 @@
+struct Rectangle {
+    hight: u32,
+    widthe: u32,
+}
+
+impl Rectangle {
+    // 以下のnewのように&selfを引数としないようなものを関連関数という
+    fn new(h: u32, w: u32) -> Rectangle {
+        Rectangle { hight: h, widthe: w }
+    }
+
+    // 以下のareaのように&selfを引数とするものをメソッドという
+    fn area(&self) ->u32 {
+        self.hight * self.widthe
+    }
+}
+
+pub fn rectangle_main() {
+    let rec = Rectangle::new(3, 5);
+
+    // 関連関数とメソッドで呼び出し方が変化することに注意
+    print!("Height={}, Width={}, Area={}", rec.hight, rec.widthe, rec.area());
+}
