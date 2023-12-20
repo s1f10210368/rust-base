b0c0a06b35795cd07aa454856957e31fb16645fb
s1f10210368 <s1f102103684@iniad.org>
2023-12-20 15:05:26 +0900

    add trait

--- practice/trait.rs
+++ practice/trait.rs
@@ -60,3 +60,43 @@ fn new_point<Z: Position>(point: &Z) {
 ジェネリクス
 「トレイト」 : 「実装対象の型 (Self)」 = N : 1
 */
+
+// ------------------------------------------------------------------
+
+// 型について
+std::convert; // <-- 型を変換するために使用するトレイト
+
+// FromとIntoなどトレイト毎に異なった変換を実施
+
+std::convert::Into; // <-- 現在のクレートの外側の型への値から値への変換
+std::convert::From; // <-- 値から値への変換
+
+// Fromトレイト
+// ある型に対して別の型からその型を作る方法を定義するようにするものです。
+pub trait From<T> {
+    fn from(T) -> Self;
+}
+
+// 例としてstrに対してStringを作る場合は次のように
+let str = "example";
+let String = String::from(str);
+
+// i32から自作のNumber型を作る場合は次のように
+struct Number {
+    value: i32,
+}
+
+impl From<i32> for Number {
+    fn from(item: i32) -> Self {
+        Number { value: item }
+    }
+}
+
+let num = Number::from(30);
+
+/*
+impl From<変換元> for 変換先 {
+    fn from(from: 変換元) -> 変換先 {
+    }
+}
+*/
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,37 @@
-// 構造体
+// 型について
+std::convert; // <-- 型を変換するために使用するトレイト
 
-struct Employee {
-    username: String,  // <-- 個々の要素をフィールドという
-    email: String,
-    employee_no: u32,
-    role: String,
+// FromとIntoなどトレイト毎に異なった変換を実施
+
+std::convert::Into; // <-- 現在のクレートの外側の型への値から値への変換
+std::convert::From; // <-- 値から値への変換
+
+// Fromトレイト
+// ある型に対して別の型からその型を作る方法を定義するようにするものです。
+pub trait From<T> {
+    fn from(T) -> Self;
 }
 
-// キー:値という形式で束縛して生成
-let emp = Employee {
-    username: String::from("yanashin18618"),
-    email: String::from("yanashin@email.com"),
-    employee_no = 1,
-    role: String::from("Engineer"),
-};
+// 例としてstrに対してStringを作る場合は次のように
+let str = "example";
+let String = String::from(str);
 
-// 構造体自体を返す関数を定義することも可能
-fn create_employee(username: &String, email: &String, employee_no: u32, role: &String) -> Employee {
-    Employee {
-        username = username.to_string(),
-        email = email.to_string(),
-        employee_no = employee_no, // <-- 引数でemployee_noを指定しているため省略が可能
-        role = role.to_string(),
+// i32から自作のNumber型を作る場合は次のように
+struct Number {
+    value: i32,
+}
+
+impl From<i32> for Number {
+    fn from(item: i32) -> Self {
+        Number { value: item }
     }
 }
+
+let num = Number::from(30);
+
+/*
+impl From<変換元> for 変換先 {
+    fn from(from: 変換元) -> 変換先 {
+    }
+}
+*/
