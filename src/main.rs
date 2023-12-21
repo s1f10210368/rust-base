c4e6b7a7276c3d00f727030a6a220f0c397017bc
s1f10210368 <s1f102103684@iniad.org>
2023-12-21 22:45:34 +0900

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,13 @@
-// 型について
-std::convert; // <-- 型を変換するために使用するトレイト
+use anyhow::Result;
+use wasm_workers_rs::{
+    handler,
+    http::{self, Request, Response},
+};
 
-// FromとIntoなどトレイト毎に異なった変換を実施
-
-std::convert::Into; // <-- 現在のクレートの外側の型への値から値への変換
-std::convert::From; // <-- 値から値への変換
-
-// Fromトレイト
-// ある型に対して別の型からその型を作る方法を定義するようにするものです。
-pub trait From<T> {
-    fn from(T) -> Self;
+#[handler]
+fn reply(req: Request<String>) -> Result<Response<String>> {
+    Ok(http::Response::builder()
+        .status(200)
+        .header("x-generated-by", "wasm-workers-server")
+        .body(String::from("Hello wasm!").into())?)
 }
-
-// 例としてstrに対してStringを作る場合は次のように
-let str = "example";
-let String = String::from(str);
-
-// i32から自作のNumber型を作る場合は次のように
-struct Number {
-    value: i32,
-}
-
-impl From<i32> for Number {
-    fn from(item: i32) -> Self {
-        Number { value: item }
-    }
-}
-
-let num = Number::from(30);
-
-/*
-impl From<変換元> for 変換先 {
-    fn from(from: 変換元) -> 変換先 {
-    }
-}
-*/
