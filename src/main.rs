72dc7b73a84139d7a0d68e5dc7e201af75f1d6f1
s1f10210368 <s1f102103684@iniad.org>
2024-01-26 23:14:58 +0900

    R

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,1 @@
-use anyhow::Result;
-use wasm_workers_rs::{
-    handler,
-    http::{self, Request, Response},
-};
 
-#[handler]
-fn reply(req: Request<String>) -> Result<Response<String>> {
-    Ok(http::Response::builder()
-        .status(200)
-        .header("x-generated-by", "wasm-workers-server")
-        .body(String::from("Hello wasm!").into())?)
-}
