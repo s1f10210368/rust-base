c9602682fbf959289cf3611a9fd8834843186cd3
s1f10210368 <s1f102103684@iniad.org>
2024-01-31 22:43:34 +0900

    R

---
+++ practice/test.rs
@@ -0,0 +1,13 @@
+use main;
+#[test]
+fn integration_test() {
+    assert_eq!(3,day_55_test::sub(day_55_test::add(3, 6), day_55_test::add(2, 4)));
+}
+
+#[test]
+fn test_add() {
+    assert!(add(1, 2) ==  3);       // 正常（ 1 + 2 == 3 -> true ）
+    assert_eq!(add(1, 2), 3);       // 正常（ 1 + 2 == 3 ）
+    assert_ne!(add(1, 2), 4);       // 正常（ 1 + 2 != 4 ）
+    assert_eq!(add(1, 2), 1);       // エラー（ 1 + 2 == 1 ）
+}
