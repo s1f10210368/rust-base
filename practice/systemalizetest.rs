d45cd4d4fd5cba858973f447d1b3c93b464a37fa
s1f10210368 <s1f102103684@iniad.org>
2024-02-01 21:32:09 +0900

    R

--- practice/systemalizetest.rs
+++ practice/systemalizetest.rs
@@ -40,3 +40,14 @@ mod tests {
         assert_eq!(3, sub(6, 3))
     }
 }
+
+/*
+assert!(x);	x が ture ならば正常、異なればパニック
+assert_eq!(x, y);	x == y ならば正常、異なればパニック
+assert_ne!(x, y);	x != y ならば正常、異なればパニック
+*/
+use day_55_test;
+#[test]
+fn integration_test() {
+    assert_eq!(3,day_55_test::sub(day_55_test::add(3, 6), day_55_test::add(2, 4)));
+}
