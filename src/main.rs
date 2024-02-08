312bd9c4efad438ab05f9dc8fc37c6b91b4952f7
s1f10210368 <s1f102103684@iniad.org>
2024-02-08 17:08:02 +0900

    rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
-fn main() {
-    let x = 3.1415;
-    let r = 8.0;
-    println!("半径:{} の円の面積は {}",r , x * r * r);
+// cargo fmt 前
+fn main(){
+    for n in 0..10{
+    println!("{n}: Hello, Rust!");
+    }
+    }
+
+// cargo fmt後
+fn main(){
+    for n in 0..10{
+    println!("{n}: Hello, Rust!");
+    }
 }
 
-// cilppy はrustのLintツール
-// プログラムを添削して修正余地があれば修正方法を提示してくれるようなもの
+// 上記のように修正してくれるツール、習慣づけたい
